a991816b9b919f041b5e8ac74113c8cf3ba4dcb2
Sergey Leskov <madarthas445@gmail.com>
2022-04-20 10:14:13 +0300

    insert new item screen

--- app/src/main/java/com/leskov/airport/presentation/insert_new_item/InsertNewItemFragment.kt
+++ app/src/main/java/com/leskov/airport/presentation/insert_new_item/InsertNewItemFragment.kt
@@ -1,9 +1,12 @@
 package com.leskov.airport.presentation.insert_new_item
 
+import android.widget.ArrayAdapter
 import androidx.fragment.app.viewModels
 import com.leskov.airport.R
 import com.leskov.airport.base.fragment.BaseVMFragment
+import com.leskov.airport.base.extensions.setOnClickWithDebounce
 import com.leskov.airport.databinding.FragmentInsertNewItemBinding
+import com.leskov.airport.domain.entity.AirportEntity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -14,4 +17,29 @@ class InsertNewItemFragment :
 
     override val viewModel: InsertNewItemViewModel by viewModels()
 
+    override fun initListeners() {
+        super.initListeners()
+
+        val items = listOf("Material", "Design", "Components", "Android")
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_dropdown_item_1line, items)
+
+        binding.tvSelectedType.setAdapter(adapter)
+
+        binding.flConfirm.setOnClickWithDebounce {
+            viewModel.insertNewItem(
+                AirportEntity(
+                    0,
+                    title = binding.title.text.toString(),
+                    type = binding.tvSelectedType.text.toString(),
+                    model = binding.model.text.toString(),
+                    loadCapacity = binding.capacity.text.toString()
+                )
+            )
+            navController.popBackStack()
+        }
+
+        binding.toolbar.setNavigationOnClickListener {
+            navController.popBackStack()
+        }
+    }
 }
--- app/src/main/java/com/leskov/airport/presentation/insert_new_item/InsertNewItemViewModel.kt
+++ app/src/main/java/com/leskov/airport/presentation/insert_new_item/InsertNewItemViewModel.kt
@@ -1,27 +1,24 @@
 package com.leskov.airport.presentation.insert_new_item
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import com.leskov.airport.base.event.Event
+import com.leskov.airport.base.event.postEvent
+import com.leskov.airport.base.live_data.SingleEventLiveData
 import com.leskov.airport.base.view_model.BaseViewModel
 import com.leskov.airport.data.repository.AirportRepository
-import com.leskov.airport.domain.AirportEntity
+import com.leskov.airport.domain.entity.AirportEntity
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class InsertNewItemViewModel @Inject constructor(private val repository: AirportRepository): BaseViewModel() {
+class InsertNewItemViewModel @Inject constructor(private val repository: AirportRepository) :
+    BaseViewModel() {
 
-    private val _newAction = MutableLiveData<Unit>()
-    val newAction: LiveData<Unit> = _newAction
-
-    fun insertNewItem(item: AirportEntity){
-        viewModelScope.launch {
-            launchSingleRequest(_newAction){
-                repository.insertItem(item)
-                postValue(Unit)
-            }
+    fun insertNewItem(item: AirportEntity) {
+        viewModelScope.launch(dispatcherController.launchInMain()) {
+            repository.insertItem(item)
         }
     }
 }
